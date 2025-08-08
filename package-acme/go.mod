7a9aaef54b1a7f69cdfd753285a911d7027ddf32
Charles Schmitt <csschmitt@gmail.com>
2025-08-08 21:46:15 +0200

    update package-acme v0.1.0

---
+++ package-acme/go.mod
@@ -0,0 +1,3 @@
+module github.com/the-schmitt/demo/package-acme/v0.1.0
+
+go 1.24.2
---
+++ package-acme/main.go
@@ -0,0 +1,7 @@
+package packageacme
+
+import "fmt"
+
+func PrintPackageAcme() {
+	fmt.Println("Hello from package-acme!")
+}
